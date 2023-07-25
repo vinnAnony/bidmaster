f5843f94aab10f02885872f856144c6b55d18eb0
Vincent Mbugua <vnjeru85@gmail.com>
2023-07-25 11:59:48 +0300

    create a channel group for multiple users

--- bidmaster/settings.py
+++ bidmaster/settings.py
@@ -64,6 +64,14 @@ TEMPLATES = [
 WSGI_APPLICATION = "bidmaster.wsgi.application"
 
 ASGI_APPLICATION = "bidmaster.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
