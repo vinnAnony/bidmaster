84422839671bcb8e7abe9ce32547b5332d8315ee
Vincent Mbugua <vnjeru85@gmail.com>
2023-07-26 09:55:21 +0300

    update admin site headers

--- bidmaster/urls.py
+++ bidmaster/urls.py
@@ -21,3 +21,7 @@ urlpatterns = [
     path("bidding/", include("bidding.urls")),
     path("admin/", admin.site.urls),
 ]
+
+admin.site.site_header = "BidMaster Admin"
+admin.site.site_title = "BidMaster Admin Portal"
+admin.site.index_title = "Welcome to BidMaster"
