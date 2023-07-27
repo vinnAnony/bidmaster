c307809095a6bd41eaf810292176a26fe435784c
Vincent Mbugua <vnjeru85@gmail.com>
2023-07-27 11:31:45 +0300

    add tailwind css and css compression

--- bidmaster/settings.py
+++ bidmaster/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +32,7 @@ INSTALLED_APPS = [
     "bidding",
     # Django channels
     "channels",
+    "compressor",
 ]
 
 MIDDLEWARE = [
@@ -48,7 +50,10 @@ ROOT_URLCONF = "bidmaster.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "bidmaster/templates",
+            BASE_DIR / "bidding/templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -120,6 +125,18 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR / "staticfiles"]
+# Add compression and caching support
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
+COMPRESS_ROOT = BASE_DIR / "static"
+COMPRESS_ENABLED = True
+STATICFILES_FINDERS = ("compressor.finders.CompressorFinder",)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- bidmaster/urls.py
+++ bidmaster/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path("bidding/", include("bidding.urls")),
     path("admin/", admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
 
 admin.site.site_header = "BidMaster Admin"
