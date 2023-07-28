765d291fd4157bba54cb7a4ef8b20d2383fe2fda
Vincent Mbugua <vnjeru85@gmail.com>
2023-07-28 13:24:58 +0300

    add sign up page

--- authentic/views.py
+++ authentic/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib import messages
 
 
 User = get_user_model()
@@ -16,7 +17,12 @@ def sign_up(request):
             form.save()
             return redirect(reverse("authentic:log_in"))
         else:
-            print(form.errors)
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"{field}: {error}")
+            
+            return redirect(reverse("authentic:sign_up"))
+
     return render(request, "authentic/signup.html", {"form": form})
 
 
